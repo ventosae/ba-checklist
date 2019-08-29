858c2ccee38b8a0c3c54cfac0f91c2897f18969a
Anton Surov <anton.surov@hotmail.com>
2019-08-29 22:42:45 +1000

    form changes

--- ba-checklist/src/componenets/form.jsx
+++ ba-checklist/src/componenets/form.jsx
@@ -1,35 +1,105 @@
 import React, { Component } from "react";
 
 class Form extends Component {
-  state = {};
+  state = {
+    projectName: " ",
+    email: " "
+  };
+
+  handleInputChange = event => {
+    this.setState({
+      projectName: event.target.value
+    });
+  };
+
+  handleEmailChange = event => {
+    this.setState({
+      email: event.target.value
+    });
+  };
+
   render() {
     return (
       <form className="form-b">
         <div class="form-group form-b__group">
-          <label for="exampleFormControlInput1 form-b__lable">
+          <label className="form-b__lable" for="project-input">
             Project Name
           </label>
           <input
             class="form-control form-b__input"
-            id="exampleFormControlInput1"
-            placeholder="Name of the Project"
+            id="project-input"
+            placeholder="Project Name"
+            onChange={this.handleInputChange}
+            value={this.state.projectName}
+          />
+          <label for="email-input form-b__lable">Your Email Address</label>
+          <input
+            class="form-control form-b__input"
+            id="email-input"
+            onChange={this.handleEmailChange}
+            placeholder="example@sportsbet.com.au"
+            value={this.state.email}
+            type="email"
           />
         </div>
+
         <div class="form-group form-b__group">
-          <label for="exampleFormControlSelect1 form-b__lable">
-            Question 1
+          <label className="form-b__lable" for="question1">
+            Subdomain/new domain: is this new content/feature located on a
+            separate domain/subdomain
           </label>
-          <select
-            class="form-control form-b__select"
-            id="exampleFormControlSelect1"
-          >
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
+          <select class="form-control form-b__select" id="question1">
+            <option>is located on separate subdomain/domain</option>
+            <option>is NOT located on subdomain/domain </option>
           </select>
         </div>
+
+        <div className="form-b__check-main">
+          <label className="form-b__lable">Web Page URL</label>
+          <div className="form-check form-b__check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="check1"
+            />
+            <label
+              className="form-check-label form-b__check-lable"
+              for="check1"
+            >
+              Make sure the new URL(s) are using keywords relevant to the page
+            </label>
+          </div>
+          <div className="form-check form-b__check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="check2"
+            />
+            <label
+              className="form-check-label form-b__check-lable"
+              for="check2"
+            >
+              URL is within the structure of the website category
+            </label>
+          </div>
+          <div className="form-check form-b__check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="check3"
+            />
+            <label
+              className="form-check-label form-b__check-lable"
+              for="check3"
+            >
+              URL doesn’t have capital letters or special symbols
+            </label>
+          </div>
+        </div>
+
         <div class="form-group form-b__group">
           <label for="exampleFormControlSelect2 form-b__lable">
             Question 2 multiple
