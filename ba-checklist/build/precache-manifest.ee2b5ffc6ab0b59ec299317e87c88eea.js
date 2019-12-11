82f1fd4662a398c5132bcdcd6542632d6f3c56b9
Anton Surov <Anton.Surov@sportsbet.com.au>
2019-12-12 10:45:55 +1100

    new deploy

--- ba-checklist/build/precache-manifest.ee2b5ffc6ab0b59ec299317e87c88eea.js
+++ ba-checklist/build/precache-manifest.ee2b5ffc6ab0b59ec299317e87c88eea.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "45b40091d284b54d2f94dff26a6a5c91",
+    "revision": "c58d4af8ae1d726431aed440dc7c72ca",
     "url": "/index.html"
   },
   {
-    "revision": "f7d48ba08f046a5aa537",
+    "revision": "4fa9b961d27972f9b176",
     "url": "/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "7577f88a40343de15785",
-    "url": "/static/css/main.75cf1750.chunk.css"
+    "revision": "6db501e631ef311c0e05",
+    "url": "/static/css/main.e1101e5e.chunk.css"
   },
   {
-    "revision": "f7d48ba08f046a5aa537",
+    "revision": "4fa9b961d27972f9b176",
     "url": "/static/js/2.8613bde4.chunk.js"
   },
   {
-    "revision": "7577f88a40343de15785",
-    "url": "/static/js/main.7e168aaf.chunk.js"
+    "revision": "6db501e631ef311c0e05",
+    "url": "/static/js/main.1320f853.chunk.js"
   },
   {
     "revision": "ef4bd80229c10653d8fe",
